2d80d30354bf683d575dd3a997a04576dd11e6db
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-03-03 10:23:48 -0300

    [ADD] add escodoo_rest_api

---
+++ escodoo_rest_api/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Escodoo Rest Api',
+    'summary': """
+        Escodoo Rest API""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    "depends": ["base_rest", "base_rest_datamodel", "component", "fleet"],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
